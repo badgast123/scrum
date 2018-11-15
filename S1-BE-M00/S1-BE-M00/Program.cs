660d7c17e5e3d3200a7e6ac561361b161bf7a3dd
badgast123 <mikagrijpma2@gmail.com>
2018-11-15 10:29:34 +0100

    Intial commit

---
+++ S1-BE-M00/S1-BE-M00/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S1_BE_M00
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
